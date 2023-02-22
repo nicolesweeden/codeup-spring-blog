110326dc0c3582d279925e205b4e8c52915b96d8
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-02-22 13:49:54 -0700

    finished post controller exercise

--- src/main/java/com/codeup/codeupspringblog/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/PostController.java
@@ -1,12 +1,32 @@
 package com.codeup.codeupspringblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String postsHomePage() {
+        return "posts index page";
+    }
 
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String postsViewSinglePost(@PathVariable long id) {
+        return "Viewing single post with id: " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postsViewFormToCreatePost() {
+        return "view form to create a post";
+    }
+
+//David said to hold off on last one since we have not talked about it yet
+//    @PostMapping("/posts/create")
+//    public String postsCreateNewPost() {
+//        return "create a new post";
+//    }
 
 }
