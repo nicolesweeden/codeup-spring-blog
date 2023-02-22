82b3f4c6a9d8e502fab62cb6b82fa683bfd82fa1
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-02-22 00:29:19 -0700

    finished mathcontroller exercise

--- src/main/java/com/codeup/codeupspringblog/MathController.java
+++ src/main/java/com/codeup/codeupspringblog/MathController.java
@@ -1,32 +1,39 @@
 package com.codeup.codeupspringblog;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class MathController {
-    @GetMapping("/")
-    public String land() {
-        return "index";
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
     }
-}
+//as an example to add 5 and 4, the url would be: http://localhost:8080/add/5/and/4
 
-//@Controller
-//public class HelloController {
-//    @GetMapping("/hello")
-//    @ResponseBody
-//    public String hello() {
-//        return "Hello, World!";
-//    }
-//
-//    @GetMapping("/hello/{name}")
-//    @ResponseBody
-//    public String sayHello(@PathVariable String name) {
-//        return "Hello, " + name + "!";
-//    }
-//
-//    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-//
-//}//end HelloController
+    @GetMapping("/subtract/{num1}/and/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+//as an example to subtract 4 from 5, the url would be: http://localhost:8080/subtract/5/and/4
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+//as an example to multiply 4 times 5, the url would be: http://localhost:8080/multiply/5/and/4
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+//as an example to divide 2 into 10, the url would be: http://localhost:8080/divide/10/and/2
+
+}//end MathController
